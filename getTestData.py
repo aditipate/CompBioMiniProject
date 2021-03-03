8514da1bb333e065c92b3f147464103e08e1140c
aditipate <you@example.com>
2021-03-03 11:54:33 -0600

    fixed syntax

--- getTestData.py
+++ getTestData.py
@@ -8,7 +8,7 @@ import os
 def getTranscriptome(SRA):
     SRA_data_address = 'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/'+ SRA + '/'+ SRA + '.1'
     wget_SRA = 'wget' + ' ' + SRA_data_address
-    fastq_dump_SRA = 'fastq-dump -I --split-files' + SRA + '.1'
+    fastq_dump_SRA = 'fastq-dump -I --split-files' + ' ' + SRA + '.1'
     os.system(wget_SRA)                                                      #retrieve small subset of input reads or testdata using wget command
     os.system(fastq_dump_SRA)                                                #uncompress data and convert to paired-end fastq files using fastq-dump command
 
