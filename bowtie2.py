1bcaf80c9002a54dda94ccaeb96d84171bc96123
aditipate <you@example.com>
2021-03-04 13:25:08 -0600

    changed file paths

--- bowtie2.py
+++ bowtie2.py
@@ -9,7 +9,7 @@ import os
 def run_bowtie2(SRRs):
     bowtie2_index = "bowtie2-build HCMV_EF999921.fasta HCMV"
     for SRR in SRRs:
-        bowtie2 = 'bowtie2 --quiet --no-unal --al-conc ' + SRR + '.fastq -x HCMV -1 ' + SRR + '.1_1.fastq -2 ' + SRR + '.1_2.fastq -S ' + SRR + '_map.sam'
+        bowtie2 = 'bowtie2 --quiet --no-unal --al-conc ' + SRR + '.fastq -x HCMV -1 ' + 'testdata/' + SRR + '.1_1.fastq -2 ' + 'testdata/' + SRR + '.1_2.fastq -S ' + SRR + '_map.sam'
         os.system(bowtie2_index)
         os.system(bowtie2)
 
@@ -25,8 +25,8 @@ def getReads(SRRs):
         elif i ==3:
             donor = "Donor 3 (6dpi)"
 
-        beforeBow_1 = open(str(SRRs[i])+'.1_1.fastq').readlines()
-        beforeBow_2 = open(str(SRRs[i])+'.1_2.fastq').readlines()
+        beforeBow_1 = open('testdata/' + str(SRRs[i])+'.1_1.fastq').readlines()
+        beforeBow_2 = open('testdata/' + str(SRRs[i])+'.1_2.fastq').readlines()
         before_reads = (len(beforeBow_1) + len(beforeBow_2))/8
 
         afterBow_1 = open(str(SRRs[i])+'.1.fastq').readlines()
