0c6be5e69758f003fc903df676e9fe55b9b36be2
aditipate <you@example.com>
2021-03-04 14:41:34 -0600

    spades + fixed format in getTestData

--- getTestData.py
+++ getTestData.py
@@ -5,10 +5,10 @@
 import os
 
 #get Donor HMCV transcriptome
-def getTranscriptome(SRRS):
-    for SRR in SRRS:
-        SRA_data_address = 'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/'+ SRR + '/'+ SRR + '.1'
-        wget_SRR = 'wget' + ' ' + SRA_data_address
+def getTranscriptome(SRRs):
+    for SRR in SRRs:
+        SRR_data_address = 'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/'+ SRR + '/'+ SRR + '.1'
+        wget_SRR = 'wget' + ' ' + SRR_data_address
         fastq_dump_SRR = 'fastq-dump -I --split-files' + ' ' + SRR + '.1'
         os.system(wget_SRR)                                                      #retrieve small subset of input reads or testdata using wget command
         os.system(fastq_dump_SRR)                                                #uncompress data and convert to paired-end fastq files using fastq-dump command
