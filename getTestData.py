54efb465faee55beb0ec45bf9ff4f13ccf3145e9
aditipate <you@example.com>
2021-03-05 10:57:03 -0600

    changed paths

--- getTestData.py
+++ getTestData.py
@@ -4,25 +4,16 @@
 
 import os
 
-#create and change to testdata folder directory
-current_path = os.getcwd()
-testdata = "/testdata"
-os.mkdir(current_path + testdata)
-os.chdir(current_path + testdata)
-
-#CURRENT DIRECTORY: .../current_path/testdata
-
 #get Donor HMCV transcriptome
 def getTranscriptome(SRRs):
+    current_path = os.getcwd()                                                   #create and change to testdata folder directory
+    testdata = "/testdata"
+    os.mkdir(current_path + testdata)
+    os.chdir(current_path + testdata)
     for SRR in SRRs:
         SRR_data_address = 'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/'+ SRR + '/'+ SRR + '.1'
         wget_SRR = 'wget' + ' ' + SRR_data_address
         fastq_dump_SRR = 'fastq-dump -I --split-files' + ' ' + SRR + '.1'
         os.system(wget_SRR)                                                      #retrieve small subset of input reads or testdata using wget command
         os.system(fastq_dump_SRR)                                                #uncompress data and convert to paired-end fastq files using fastq-dump command
-
-
-#change to current directory
-os.chdir(current_path)
-
-#CURRENT DIRECTORY: .../current_path
+        os.chdir(current_path)                                                   #change to current directory
